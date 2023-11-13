29a79dc1716a1b1a16c2817b92ab9005e0e9ad2e
zhuxingyu2021 <93047415+zhuxingyu2021@users.noreply.github.com>
2023-11-13 20:36:19 +0800

    amazon_s3

--- scripts/ingest-data.ts
+++ scripts/ingest-data.ts
@@ -10,23 +10,79 @@ import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE } from '@/config/pinecone';
 import { MONGO_DBNAME, MONGO_ATLAS_VECTOR_SEARCH_INDEX_NAME, MONGO_COLLECTION_NAME } from '@/config/mongo';
 import { REDIS_INDEX_NAME } from '@/config/redis';
 import { DATABASE_TYPE } from '@/config/common';
-import { DirectoryLoader } from 'langchain/document_loaders/fs/directory';
 import { mongoCli } from '@/utils/mongo-client';
+import { S3Client, ListObjectsV2Command, GetObjectCommand } from "@aws-sdk/client-s3";
+import { Readable } from 'stream';
 
-/* Name of directory to retrieve your files from 
-   Make sure to add your PDF files inside the 'docs' folder
-*/
-const filePath = 'docs';
+
+const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID??''
+const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY??''
+const AWS_REGION = process.env.AWS_REGION??''
+const AWS_BUCKET = process.env.AWS_BUCKET??''
+
+const s3Client = new S3Client({
+   region: AWS_REGION ,
+   credentials: {
+    accessKeyId: AWS_ACCESS_KEY_ID,
+    secretAccessKey: AWS_SECRET_ACCESS_KEY
+  }
+});
+
+async function listPdfFiles(bucketName: string): Promise<string[]> {
+  const params = {
+    Bucket: bucketName,
+  };
+
+  let isTruncated = true;
+  let continuationToken: string | undefined;
+  let pdfFiles: string[] = [];
+
+  while (isTruncated) {
+    const { Contents, IsTruncated, NextContinuationToken } = await s3Client.send(
+      new ListObjectsV2Command({ ...params, ContinuationToken: continuationToken })
+    );
+
+    // Filter and collect the PDF files
+    const pdfKeys = Contents?.filter((item) => item.Key?.endsWith('.pdf')).map((item) => item.Key!) || [];
+    pdfFiles = pdfFiles.concat(pdfKeys);
+
+    isTruncated = IsTruncated || false;
+    continuationToken = NextContinuationToken;
+  }
+
+  return pdfFiles;
+}
+
+async function downloadPdfFile(bucketName: string, key: string): Promise<Blob> {
+  const command = new GetObjectCommand({
+    Bucket: bucketName,
+    Key: key,
+  });
+
+  const { Body } = await s3Client.send(command);
+
+  if (Body instanceof Readable) {
+    // Convert the Node stream to a Blob
+    return new Promise((resolve, reject) => {
+      const chunks: Uint8Array[] = [];
+      Body.on("data", (chunk) => chunks.push(chunk));
+      Body.on("end", () => {
+        const blob = new Blob(chunks, { type: 'application/pdf' });
+        resolve(blob);
+      });
+      Body.on("error", reject);
+    });
+  } else {
+    // Handle non-stream body types here, if necessary
+    throw new Error("Expected a stream for the S3 object body.");
+  }
+}
 
 export const run = async () => {
   try {
-    /*load raw docs from the all files in the directory */
-    const directoryLoader = new DirectoryLoader(filePath, {
-      '.pdf': (path) => new PDFLoader(path),
-    });
-
-    // const loader = new PDFLoader(filePath);
-    const rawDocs = await directoryLoader.load();
+    const s3Keys = await listPdfFiles(AWS_BUCKET);
+    const s3Objs = await Promise.all(s3Keys.map(key => downloadPdfFile(AWS_BUCKET, key)));
+    const rawDocs = (await Promise.all(s3Objs.map(obj => new PDFLoader(obj).load()))).flat();
 
     /* Split text into chunks */
     const textSplitter = new RecursiveCharacterTextSplitter({
